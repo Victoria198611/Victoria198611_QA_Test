cc1e185990ecc5df74dfa5765facfaa6e380ea5c
Victoria Vutcariov <vutcariov5@mail.ru>
2025-09-16 11:35:18 +0300

    "Corectat bucla while pentru afișarea numerelor pare"

--- src/Ex.java
+++ src/Ex.java
@@ -1,11 +1,12 @@
 public class Ex {
-public static void main (String[]args){
-int i=10;
-while (i % 2==0) {
+    public static void main(String[] args) {
+        int i = 10;
 
-System.out.println(i);
-i++;
-}
-while (i<=10);
-}
+        while (i >= 0) { // continuă până la 0
+            if (i % 2 == 0) { // verifică dacă e par
+                System.out.println(i);
+            }
+            i--; // scade cu 1
+        }
+    }
 }
